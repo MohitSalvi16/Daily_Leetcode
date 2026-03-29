4266a9ceb01a5ee6eed8bad81ac4440938bc8c20
MohitSalvi16 <72203459+MohitSalvi16@users.noreply.github.com>
2026-03-30 02:05:58 +0530

    LeetCode: Solved 2999-Check if Strings Can be Made Equal With Operations I

---
+++ LeetCode/easy/2999-check-if-strings-can-be-made-equal-with-operations-i/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+        let ans=false; 
+        if(s1[0]==s2[0] && s1[2]==s2[2] && s1[1]==s2[1] && s1[3]==s2[3]){
+            ans=true;
+        }
+        else if(s1[0]==s2[0] && s1[2]==s2[2] && s1[1]==s2[3] && s1[3]==s2[1]){
+        ans=true;
+        }
+        else if(s1[0]==s2[2] && s1[2]==s2[0] && s1[1]==s2[1] && s1[3]==s2[3]){
+        ans=true;
+        }
+        else if(s1[0]==s2[2] && s1[2]==s2[0] && s1[1]==s2[3] && s1[3]==s2[1]){
+        ans=true;  
+        }
+        return ans;
+};
