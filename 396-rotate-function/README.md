6bbda1d8356423c385ed5466ef704bb5936f6934
MohitSalvi16 <72203459+MohitSalvi16@users.noreply.github.com>
2026-05-01 22:47:56 +0530

    LeetCode: Solved 396-Rotate Function

---
+++ 396-rotate-function/README.md
@@ -0,0 +1,18 @@
+# 396. Rotate Function
+
+| Detail | Value |
+|--------|-------|
+| **Difficulty** | Medium |
+| **Tags** | `Array`, `Math`, `Dynamic Programming` |
+| **Language** | cpp |
+| **Runtime** | 0 ms |
+| **Memory** | 99.4 MB |
+| **Synced** | 2026-05-01T17:17:50.415Z |
+
+## Links
+
+- [LeetCode Problem](https://leetcode.com/problems/rotate-function/)
+
+---
+
+_Auto-synced by LeetFlow Chrome Extension_
---
+++ 396-rotate-function/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n=nums.size();
+
+        int sum=0;
+        int F=0;
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            F=F+i*nums[i];
+        }
+
+        int result=F;
+
+        for(int k=0;k<=n-1;k++){
+            int Fnew=F+sum-n*nums[n-1-k];
+            result=max(result,Fnew);
+            F=Fnew;
+        }
+
+        return result;
+
+    }
+};
