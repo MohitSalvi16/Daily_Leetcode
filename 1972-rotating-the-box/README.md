b592819f9973ce157be5e2b294512b08692af02f
MohitSalvi16 <72203459+MohitSalvi16@users.noreply.github.com>
2026-05-06 22:15:13 +0530

    LeetCode: Solved 1972-Rotating the Box

---
+++ 1972-rotating-the-box/README.md
@@ -0,0 +1,18 @@
+# 1972. Rotating the Box
+
+| Detail | Value |
+|--------|-------|
+| **Difficulty** | Medium |
+| **Tags** | `Array`, `Two Pointers`, `Matrix` |
+| **Language** | cpp |
+| **Runtime** | 60 ms |
+| **Memory** | 56.9 MB |
+| **Synced** | 2026-05-06T16:45:08.744Z |
+
+## Links
+
+- [LeetCode Problem](https://leetcode.com/problems/rotating-the-box/)
+
+---
+
+_Auto-synced by LeetFlow Chrome Extension_
---
+++ 1972-rotating-the-box/solution.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m=boxGrid.size();
+        int n=boxGrid[0].size();
+        
+        vector<vector<char>> ans(n,vector<char>(m));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[i][j]=boxGrid[j][i];
+            }
+        }
+
+        for(vector<char>& row : ans){
+            reverse(begin(row),end(row));
+        }
+
+
+        for(int j=0;j<m;j++){
+            for(int i=n-1;i>=0;i--){
+             if(ans[i][j]=='.'){
+                int stoneRow=-1;
+
+                for(int k=i-1;k>=0;k--){
+                    if(ans[k][j]=='*'){
+                        break;
+                    }
+                    else if(ans[k][j]=='#'){
+                    stoneRow=k;
+                    break;
+                    }
+                }
+
+                if(stoneRow!=-1){
+                 ans[i][j]='#';
+                 ans[stoneRow][j]='.';
+                }
+             }
+            }
+        }
+        return ans;
+    }
+};
